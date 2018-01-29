b1c31d93954b51849a096b127a50baa0cf55202d
Long <hxscript@163.com>
2018-01-29 19:58:12 +0800

    add

--- NODE_demo/album/app.js
+++ NODE_demo/album/app.js
@@ -1,10 +1,11 @@
 // Copyright (c) 2018 by HxScript. All Rights Reserved.
 var express = require("express")
 var app = express()
+var ejs = require("ejs")
 var router = require("./controller/router.js")
 
 //设置模板引擎
-app.set("view emgine","ejs")
+app.set("view engine","ejs")
 
 // 提供静态服务
 app.use('/static', express.static('public'));
--- NODE_demo/album/controller/router.js
+++ NODE_demo/album/controller/router.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2018 by HxScript. All Rights Reserved.
 exports.showIndex = function(req,res) {
-  res.send("我是首页")
+  res.render("index",{
+    "albums":["aa", "bb" ,"cc"]
+  })
 }
 
-// exports.showAlbum = function(req,res) {
-//   res.send("相册：" + req.parems.albumName)
-// }
+exports.showAlbum = function(req,res) {
+  res.send("相册：" + req.params.albumName)
+}
